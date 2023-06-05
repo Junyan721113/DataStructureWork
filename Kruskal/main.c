ffe30d825101fa69609c963982f8edb161056b5e
geotle77 <125753377+geotle77@users.noreply.github.com>
2023-06-05 23:38:06 +0800

    Update main.c
    Add UI

--- Kruskal/main.c
+++ Kruskal/main.c
@@ -1,6 +1,15 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include<stdio.h>
-#include"Datastruct.h"
+#include<stdlib.h>
+#include<string.h>
+void InitEGraph(EGraph *g,int vexnum,int edgenum) {
+	if (vexnum > MAX_VEX || edgenum > MAX_EDGE)exit(-1);
+	g->vexnum = vexnum; g->edgenum = edgenum;
+	memset(g->edgelist, 0, edgenum);
+	for (int i = 0; i < vexnum; i++) {
+		g->vexlist[i] = i+1;
+	}
+}
 MSTEdge* Kruskal(EGraph* g) {
 	int i, j, k, s1, s2;
 	MSTEdge* TE;
@@ -9,7 +18,7 @@ MSTEdge* Kruskal(EGraph* g) {
 	vSet = (int*)malloc(g->vexnum * sizeof(int));
 	for (i = 0; i < g->vexnum; i++) vSet[i] = i;//初始化数组vSet
 	//对边按权值从小到大排序
-	BubbleSort(g->edgelist, g->edgenum);
+	InsertSort(&g->edgelist, g->edgenum);
 	i = 0; j = 0;
 	while (i < g->vexnum && j < g->edgenum) {
 		s1 = vSet[g->edgelist[j].ivex];
@@ -27,10 +36,46 @@ MSTEdge* Kruskal(EGraph* g) {
 	free(vSet);
 	return TE;
 }
-void BubbleSort(ElemType s[], int n) {
-
+void InsertSort(ENode *a, int len)
+{
+	int i, j;
+	ENode temp;
+	for (i = 1; i < len; i++)
+	{
+		temp.ivex = a[i].ivex;
+		temp.jvex = a[i].jvex;
+		temp.weight = a[i].weight;
+		//当前数小于前一位数时
+		if (a[i].weight< a[i - 1].weight)
+		{
+			//将子序列重新排列为有序序列
+			for (j = i - 1; temp.weight< a[j].weight; j--)
+			{
+				a[j + 1] = a[j];
+			}
+			a[j + 1].ivex= temp.ivex;
+			a[j + 1].jvex = temp.jvex;
+			a[j + 1].weight = temp.weight;
+		}
+	}
 }
-
 int main() {
+	EGraph G;
+	int vexnum, edgenum;
+	int a, b,c;
+	scanf("%d %d", &vexnum, &edgenum);
+	InitEGraph(&G,vexnum,edgenum);
+	char tmp[CACHESIZE], * tmpp = tmp;
+	int i = 0, offset;
+	fgets(tmp, CACHESIZE, stdin);
+	while (sscanf(tmpp, "%d-%d %d%n", &a,&b,&c, &offset) == 2)
+	{
+		tmpp += offset + 1;
+		G.edgelist[i].ivex = a;
+		G.edgelist[i].jvex = b;
+		G.edgelist[i].weight = c;
+		i++;
+		if (i >= edgenum)break;
+	}
 
 }
