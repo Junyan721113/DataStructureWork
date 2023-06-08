e95160e36a911a6b3b1114b9535b5fb48a466c9e
geotle77 <125753377+geotle77@users.noreply.github.com>
2023-06-08 21:02:57 +0800

    Update Datastruct.h
    Compele Data Structure!

--- Kruskal/Datastruct.h
+++ Kruskal/Datastruct.h
@@ -1,21 +1,19 @@
-#define MAX_VEX 10
-#define MAX_EDGE 50
-#define MAX_TREE_SIZE 50
-#define CACHESIZE 100
 typedef int ElemType;
+typedef int Status;
 typedef struct Enode {
 	int  ivex, jvex; //边所依附的两个顶点
 	int  weight; //边的权值 
 } ENode; //边表元素类型定义
 typedef struct {
 	int  vexnum, edgenum; //顶点数和边数
-	ElemType vexlist[MAX_VEX];   //顶点表
+	ElemType vexlist[MAX_VEX];   //顶点表其中值存放度数
 	ENode  edgelist[MAX_EDGE];  //边表
 } EGraph;    //用边表存放图
 typedef struct MSTEdge {
-	int vex1, vex2, weight; //边所依附的两个顶点及权重
+	ENode  edgelist[MAX_EDGE]; //边所依附的两个顶点及权重
+	int n;
+	int boot;
 } MSTEdge; //存放最小生成树
-
 typedef struct PTNode {
 	ElemType data;
 	int parent;
